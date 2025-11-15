3c7579a96fee801cf9feaee37b7e3ef52cdf2f67
Floriane Diem <florianediem@gmail.com>
2025-11-15 16:41:49 -0500

    eventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,50 @@
 class EventsController < ApplicationController
-  # comment
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    if @event.update(topic_params)
+      redirect_to event_path
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to topics_path, status: :see_other
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :max_capacity, :date, :time)
+  end
 end
