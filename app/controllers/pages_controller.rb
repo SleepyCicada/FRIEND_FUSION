e2f88a5a311e5e3bec774632ef4dd5ee8acfe943
Pavel Mikhailau <paul.mikhailau@gmail.com>
2025-11-20 21:25:19 -0500

    home page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,12 @@ class PagesController < ApplicationController
 
   def home
     @topics = [
-      OpenStruct.new(title: "Topic 1", description: "The language that borrowed from everyone, forgot to return it, and now makes the rules up as it goes.", badge: "English", image_class: "bg-topic-1"),
-      OpenStruct.new(title: "Topic 2", description: "La langue de l’amour, des croissants, et de l’élégance — même quand on commande juste du fromage.", badge: "Français", image_class: "bg-topic-2"),
-      OpenStruct.new(title: "Topic 3", description: "Rápido, ardiente y lleno de estilo — como bailar salsa con la lengua.", badge: "Español", image_class: "bg-topic-3"),
-      OpenStruct.new(title: "Topic 4", description: "優雅で効率的、時に謎めいている — 一文字で哲学を語れる言語。", badge: "Japanise", image_class: "bg-topic-4"),
-      OpenStruct.new(title: "Topic 5", description: "Ogni frase sembra un’opera appassionata — anche se stai solo chiedendo indicazioni.", badge: "Italiano", image_class: "bg-topic-5"),
-      OpenStruct.new(title: "Topic 6", description: "Ünlüler coşkuyla dans eder, ekler kebap gibi üst üste dizilir — lezzetli ve ifade dolu.", badge: "Türkçe", image_class: "bg-topic-6")
+      OpenStruct.new( description: "The language that borrowed from everyone, forgot to return it, and now makes the rules up as it goes.", badge: "English", image_class: "bg-topic-1"),
+      OpenStruct.new( description: "Even their sarcasm sounds like it’s been aged in oak barrels and served with cheese.", badge: "Français", image_class: "bg-topic-2"),
+      OpenStruct.new( description: "Passionate, rhythmic, and always ready to dance. Rolls their R’s like maracas.", badge: "Español", image_class: "bg-topic-3"),
+      OpenStruct.new( description: "Polite, precise, and secretly a pun master. Brings origami and philosophical haikus.", badge: "Japanese", image_class: "bg-topic-4"),
+      OpenStruct.new( description: "Speaks with their hands, sings their sentences, and turns every conversation into an opera.", badge: "Italiano", image_class: "bg-topic-5"),
+      OpenStruct.new( description: "Consonant gymnastics. Can say “przyszczyna” without blinking.", badge: "Polski", image_class: "bg-topic-6")
     ]
     @events = Event.all
   end
