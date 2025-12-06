6f2e8bbf207dc90262d3d9929661e8ad9cf7dc15
Pavel Mikhailau <paul.mikhailau@gmail.com>
2025-12-06 13:59:43 -0500

    Favicon added, pages 404,500 added

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,7 @@ module FriendFusion
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.exceptions_app = self.routes
+
   end
 end
